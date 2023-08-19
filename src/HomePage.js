01900d2ba1121555ee6e70c5360f856c09b2a572
Lee-Chandler <lining07@bilibili.com>
2023-08-19 18:57:33 +0800

    使用接口返回的url

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,38 +4,40 @@ import Video from './Video';
 import './HomePage.css';
 import logo from './logo.png';
 import axios from 'axios'; 
-import { useEffect } from 'react';
 const HomePage = () => {
-  let url = ''
+  const [url, setUrl] = React.useState('');
   const [isReady, setIsReady] = React.useState(false);
   const navigateToSecondPage = () => {
     window.location.href = '/second?url=' + url;
   };
 
-  const getVideoUrl = () => {
-    axios.get('/video').then(res => {
-      console.log(res);
-      return 'res';
-    }).catch(err => {
-      console.log(err);
-      return 'err';
-    })
-  }
-
-  url = getVideoUrl();
-  console.log(url);
-
-  useEffect(() => {
-    if (url) {
-      setIsReady(true);
+  const getVideoUrl = async () => { // 注意函数声明前面的 async
+    try {
+      const res = await axios.get('http://localhost:4000/api/srcVid');
+      return res.data.url;
+    } catch (err) {
+      console.error(err);
+      return '';
     }
-  }, [url]);
+  }
+  
+  const setVideoUrl = async () => {
+    try {
+      const res = await getVideoUrl(); // 使用 await 等待 getVideoUrl 函数的结果
+      setUrl(res);
+      setIsReady(true);
+    } catch (err) {
+      console.error(err);
+    }
+  }  
+
+  setVideoUrl();
 
   return (
     <div>
       <img className='logo' src={logo} />
       <div className='src-video'>
-        <Video/>
+        <Video src={url} />
       </div>
       {isReady && <button className='jump-btn' onClick={ navigateToSecondPage } />}
     </div>
--- src/Video.js
+++ src/Video.js
@@ -1,23 +1,27 @@
 import React, { useState, useRef,useEffect } from 'react';
 import videoFile from './test.mp4';
 
-const Video = (src,paused,onVideoChanged) => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null);
-
-  // useEffect(()=>{
-  //   console.log(paused,'666')
-  // },[paused])
-  const videoButtonCheck=()=>{
-    if (videoRef.current.paused) {
-      videoRef.current.play();
-      // setIsPlaying(true);
-    } else {
-      videoRef.current.pause();
-      // setIsPlaying(false);
+  const Video = (src,paused,onVideoChanged) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const videoRef = useRef(null);
+  
+    // useEffect(()=>{
+    //   console.log(paused,'666')
+    // },[paused])
+    const videoButtonCheck=()=>{
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+        // setIsPlaying(true);
+      } else {
+        videoRef.current.pause();
+        // setIsPlaying(false);
+      }
     }
-  }
-
+  const [url, setUrl] = useState('');
+  useEffect(() => {
+    console.log(src);
+    setUrl(src);
+  }, [src])
   return (
     <div>
       <video ref={videoRef} src={videoFile} controls={false} style={{width:'100%',height:'100%',borderRadius:'15px'}}/>
@@ -26,6 +30,6 @@ const Video = (src,paused,onVideoChanged) => {
       }}></button>
     </div>
   );
-};
+}
 
 export default Video;
